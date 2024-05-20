1e2adad8c5368ab6b6ca1e9de3813e330b7f7734
Bodomrus70@mail.ru <Bodomrus70@mail.ru>
2024-05-20 23:29:22 +0700

    Добавьте файлы проекта.

---
+++ Module_3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Artist;
+
+class Program
+
+{
+
+    static void Main(string[] args)
+    {
+        string MyName;
+
+        MyName = "Dima";
+
+        Console.WriteLine(MyName);
+
+        Console.ReadKey();
+    }
+
+}
